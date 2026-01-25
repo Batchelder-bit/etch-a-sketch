951dfc07cb9fc7b69beffbebc66c578c9a4f7d7c
Citrus <bbatchelder@proton.me>
2026-01-24 21:05:11 -0700

    Grid now generates properly. Started my css and js over as its been awhile.

--- java-script_v2.js
+++ java-script_v2.js
@@ -1,2 +1,75 @@
 console.log("I am working");
 
+const container = document.querySelector('.container');
+
+function createRow () {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('grid');
+    return newDiv;
+}
+
+function createBoxes (numberofBoxes) {
+    let currentRow = container.lastElementChild;
+    for (let i = 0; i < numberofBoxes; i++) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('box');
+        currentRow.appendChild(newDiv);
+    }
+    
+}
+
+function returnClosestSquareRoot (number) {
+    if (number < 0 || isNaN(number))
+        return NaN;
+
+    squareRoot = Math.sqrt(number);
+    roundedSquareRoot = Math.round(squareRoot);
+    return roundedSquareRoot;
+}
+
+function createAllRows(inputNumber) {
+    for (let i = 0; i < inputNumber; i++) {
+        newRow = createRow();
+        container.appendChild(newRow);
+    }
+}
+
+function createAllBoxes(inputNumber) {
+    for (let i = 0; i < inputNumber; i++) {
+        newBox = createBox();
+        let currentRow = document.querySelector('.grid');
+        currentRow.appendChild(newBox);
+    }
+}
+
+function createRowandBox (inputNumber) {
+    oneRow = createAllRows(1);
+    for (let i = 0; i < inputNumber; i++) {
+        boxes = createAllBoxes(1);
+    }
+}
+
+/// function main (userInput) {
+///    for (let i = 0; i < userInput; i++) {
+///        createRowandBox (userInput);
+///    }
+///} 
+
+function createGrid (userEval) {
+    for (let i = 0; i < userEval; i++) {
+        let row = createRow();
+        if (i == 0) 
+        continue
+        else {
+            container.appendChild(row);
+        }
+        createBoxes(userEval);
+    }
+}
+
+function changeMouseColor () {
+    
+}
+
+
+document.addEventListener('DOMContentLoaded', () => createGrid(5));
